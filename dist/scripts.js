37b94da9680303014a3e6bb25890120e915b5a88
mircobrb <55443471+Mircobrb@users.noreply.github.com>
2024-08-06 17:06:29 +0200

    added links to buttons

---
+++ dist/scripts.js
@@ -0,0 +1,9 @@
+// Write to clipboard
+
+async function writeClipboardText(text) {
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
